f70b9426f9e576b64ccba672f5891b68a2bfdd2a
committed from porthos on board cpu <invalid@invalid>
2017-02-08 13:34:13 +0100

    uncrustify

--- flight/libraries/inc/insgps.h
+++ flight/libraries/inc/insgps.h
@@ -60,7 +60,7 @@ void INSCorrection(float mag_data[3], float Pos[3], float Vel[3], float BaroAlt,
 
 void INSResetP(float PDiag[13]);
 void INSGetP(float PDiag[13]);
-void INSGetPAddress(float* address[13]);
+void INSGetPAddress(float *address[13]);
 void INSSetState(float pos[3], float vel[3], float q[4], float gyro_bias[3], float accel_bias[3]);
 void INSSetPosVelVar(float PosVar[3], float VelVar[3]);
 void INSSetGyroBias(float gyro_bias[3]);

93f49f813eeb34412a837c8ce308488fe4ace389
Eric Price <eprice@peene.is.localnet>
2017-09-19 18:24:33 +0200

    Added AIRCAP howto and code-file-license headers

--- ground/TelemetryBridge/usbhid_tcp_bridge_dump.c
+++ ground/TelemetryBridge/usbhid_tcp_bridge_dump.c
@@ -1,5 +1,26 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * Additional note on redistribution: The copyright and license notices above
+ * must be maintained in each individual source file that is a derivative work
+ * of this source file; otherwise redistribution is prohibited.
+ */
 /**
  * This is a small test program that connects to OpenPilot/CC via USB and writes a .opl compatible stream to stdout
+ * @author     The LibrePilot Project, http://www.librepilot.org Copyright (C) 2016.
+ *             Max Planck Institute for intelligent systems, http://www.is.mpg.de Copyright (C) 2016.
  */
 
 #include <usb.h>

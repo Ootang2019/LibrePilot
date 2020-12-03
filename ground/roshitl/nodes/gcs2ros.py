97b201b0a9a17c7c2f28266453c5ea7860ea1c96
Eric Price <eric.price@tuebingen.mpg.de>
2020-12-03 21:06:22 +0100

    updated ros packages to use correct message type for HITL actuators

--- ground/roshitl/nodes/gcs2ros.py
+++ ground/roshitl/nodes/gcs2ros.py
@@ -2,12 +2,14 @@
 
 import rospy
 import socket
-from std_msgs.msg import Float64MultiArray,MultiArrayDimension
+from std_msgs.msg import MultiArrayDimension
+from librepilot.msg import LibrepilotActuators
 
 #from geometry_msgs.msg import PoseStamped
+sequence=0
 
 server = ("127.0.0.1",40000)
-topic = ('GCSACTUATORS',Float64MultiArray)
+topic = ('GCSACTUATORS',LibrepilotActuators)
 
 rospy.init_node('GCS2ROS')
 rospy.loginfo("Startup")
@@ -17,16 +19,22 @@ UDPSocket.bind(server)
 msgpublisher = rospy.Publisher(topic[0],topic[1],queue_size=3)
 rospy.loginfo("socket bound, publisher initialized")
 
+
 def NetworkHandler(values):
 
     #12 actuator channels, send as multiarray
+    global sequence
 
-    msg=Float64MultiArray()
-    msg.layout.dim=[MultiArrayDimension()]
-    msg.layout.dim[0].label="Channel"
-    msg.layout.dim[0].size=12
-    msg.layout.dim[0].stride=1
-    msg.data=values
+    msg=LibrepilotActuators()
+    msg.header.frame_id="world"
+    msg.header.seq = sequence
+    sequence+=1
+    msg.header.stamp=rospy.Time.now()
+    msg.data.layout.dim=[MultiArrayDimension()]
+    msg.data.layout.dim[0].label="Channel"
+    msg.data.layout.dim[0].size=12
+    msg.data.layout.dim[0].stride=1
+    msg.data.data=values
     msgpublisher.publish(msg)
     rospy.loginfo("msg sent to ros")
     return
